bf7b1fd83748beee896ac9bdda89d40b59dd6b57
biacosta7 <contact.beatrizcosta@gmail.com>
2024-09-27 12:09:21 -0300

    view e model de centros

--- centros/views.py
+++ centros/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CentroColetaForm
 from django.contrib.auth.decorators import login_required
 from .models import CentroColeta
@@ -19,3 +19,17 @@ def cadastrar_centro(request):
 def lista_centros(request):
     centros = CentroColeta.objects.all() 
     return render(request, 'centros/lista_centros.html', {'centros': centros})
+
+
+@login_required
+def update_centros(request, centro_id):
+    centro = get_object_or_404(CentroColeta, id=centro_id, usuario_responsavel=request.user)
+    if request.method == 'POST':
+        form = CentroColetaForm(request.POST, instance=centro)
+        if form.is_valid():
+            form.save()
+            return redirect('centros:lista_centros')  # Redirecionar para a lista de centros
+    else:
+        form = CentroColetaForm(instance=centro)
+
+    return render(request, 'centros/update_centro.html', {'form': form})
